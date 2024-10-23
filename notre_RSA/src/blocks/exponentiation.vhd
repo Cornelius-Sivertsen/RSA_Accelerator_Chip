a5a4b627abfe18c91291d7674cc66b6caf734c5f
Thibault Pouyanne <93395244+Tezyy@users.noreply.github.com>
2024-10-23 16:53:34 +0200

    update

--- notre_RSA/src/blocks/exponentiation.vhd
+++ notre_RSA/src/blocks/exponentiation.vhd
@@ -32,30 +32,34 @@ end exponentiation;
 
 
 architecture expBehave of exponentiation is
-begin
     -- registers
     signal E_r              : std_logic_vector(C_block_size-1 downto 0); --register of exponent
     signal C_r              : std_logic_vector(C_block_size-1 downto 0); --register of C
     signal E_nxt            : std_logic_vector(C_block_size-1 downto 0);
     signal C_nxt            : std_logic_vector(C_block_size-1 downto 0);
 
+    signal trigger_reg          : std_logic; -- register for the trigger signal from the FSM
+
+
     -- Blakely signals
-    signal Sortie_blk_1     : std_logic_vector(C_block_size-1downto 0);
-    signal Sortie_blk_2     : std_logic_vector(C_block_size-1downto 0);
+    signal Sortie_blk_1     : std_logic_vector(C_block_size-1 downto 0);
+    signal Sortie_blk_2     : std_logic_vector(C_block_size-1 downto 0);
 
     signal Ready_blk_1      :std_logic;
     signal Ready_blk_2      :std_logic;
 
-    signal trigger          : std_logic; -- MUX sur Ready_blk_1 and Ready_blk_2 en fonction de ei
 
-    --FSM signal
-    signal step             : std_logic_vector(C_block_size-1 downto 0);
+    signal MSF_exponent     :std_logic;
+    --FSM
+    signal fill_E_and_C      :std_logic; 
+    signal calculation_finished : std_logic;
 
-    -- 
+    
+    
 
-	result <= message xor modulus;
-	ready_in <= ready_out;
-	valid_out <= valid_in;
+	--result <= message xor modulus;
+	--ready_in <= ready_out;
+	--valid_out <= valid_in;
 
     begin
         BLACKLEY_1 : entity work.Blackley
@@ -66,11 +70,11 @@ begin
                 In_Blakley_Value1           => C_r      ;
                 In_Blakley_Value2           => C_r      ;
                 In_Blakley_Mod              => n        ;
-                Manual_Reset                => -- FINIR
-                Input_Ready                 => Ready_blk_1;
+                --Manual_Reset                => -- FINIR
+                Input_Ready                 => trigger_reg;
 
                 Out_Blackley :              => Sortie_blk_1;
-                Result_ready                => Ready_blk_2 ;
+                Result_ready                => Ready_blk_1 ;
             );
         BLACKLEY_2 : entity work.Blackley
             port map (
@@ -80,26 +84,51 @@ begin
                 In_Blakley_Value1           => Sortie_blk_1;
                 In_Blakley_Value2           => message     ;
                 In_Blakley_Mod              => n        ;
-                Manual_Reset                => -- FINIR
-                Input_Ready                 => Ready_blk_1;
+                --Manual_Reset                => -- FINIR
+                Input_Ready                 => Ready_blk_1 and MSF_exponent;
 
                 Out_Blackley :              => Sortie_blk_2;
-                Result_ready                => -- FINIR ;
+                Result_ready                => Ready_blk_2 ;
+            );
+
+        REGISTER_E : entity work.register_shift_left
+            port map(
+                CLK       => clk;
+                RESET     => reset_n;
+                CONTROL => fill_E_and_C;
+                TRIG    => trigger_reg;
+                DATA_IN  => key;
+                MSF_OUT  => MSF_exponent;
+            );
+        
+        FSM : entity work.FSM --CHANGE NAME
+            port map(
+                reset => reset_n;
+                clock => clk;
+                trigger => trigger_reg;
+                calculation_finished =>calculation_finished;
+                write_back_resolution => fill_E_and_C;
             );
 
             -- FINIR
             -- FSM1 : entity work. 
 
         -- process des registers
-        process(clk, reset_n, trigger)
+        process(clk, reset_n)
             begin
                 if (reset_n = '0') then
                     C_r <= (0 => '1', others => '0');
-		        elsif (rising_edge(clk) and trigger='1') then
+		        elsif (rising_edge(clk)) then
+                    if (trigger='1') then
                     C_r <= C_nxt;
+                    end if;
+                    
                 end if;
         end process;
+        
 
 
-            
+        
+                
+                    
 end expBehave;
