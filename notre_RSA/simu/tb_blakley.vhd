a95fb6ef396b59346aea4acea64db0fbff633e7d
Arthus D <arthus.dz@gmail.com>
2024-11-05 09:18:33 +0100

    Création 2 archi + testbench

---
+++ notre_RSA/simu/tb_blakley.vhd
@@ -0,0 +1,159 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
+entity test_tb_blakley is
+end entity test_tb_blakley;
+
+architecture TEST of test_tb_blakley is
+    constant Period : time := 1 ns;
+    signal Done : boolean;
+
+    signal T_CLK                        :  std_logic := '0';
+    signal T_RST                        :  std_logic;
+    signal T_Blakley_Value1, T_Blakley_Value2, T_Blakley_Mod :  std_logic_vector(255 downto 0);
+    signal T_InReady : std_logic;
+
+    signal T_OutBlackley : std_logic_vector(255 downto 0);
+    signal T_Outready : std_logic;    
+
+    signal OK, OK_general : boolean := true;
+
+    signal expected_result : unsigned(255 downto 0);
+
+begin
+
+    T_CLK <= '0' when Done else not T_CLK after Period;
+    T_RST <= '0';
+
+    UUT: entity work.Blackley(blackley_archi_v1) port map (
+        CLOCK => T_CLK,
+        RESET => T_RST,
+        In_Blakley_Value1 => T_Blakley_Value1,
+        In_Blakley_Value2 => T_Blakley_Value2,
+        In_Blakley_Mod => T_Blakley_Mod,
+        Input_Ready => T_InReady,
+        Out_Blackley => T_OutBlackley,
+        Result_ready => T_Outready
+    );
+
+    process
+        variable result : integer;
+        variable ok_v : boolean := false;
+    begin
+        Done <= false;
+
+        -- Test Case 1 a: Simple case
+        T_Blakley_Value1 <= X"0000000000000000000000000000000000000000000000000000000000000000";
+        T_Blakley_Value2 <= X"0000000000000000000000000000000000000000000000000000000000000000";
+        T_Blakley_Mod <= (others => '1');
+        T_InReady <= '1';
+        wait for 2 ns;
+        T_InReady <= '0';
+        wait until T_Outready = '1';
+        assert T_OutBlackley = X"0000000000000000000000000000000000000000000000000000000000000000" report "Test Case 1 Failed" severity warning;
+        OK <= T_OutBlackley = X"0000000000000000000000000000000000000000000000000000000000000000";
+        ok_v := T_OutBlackley = X"0000000000000000000000000000000000000000000000000000000000000000";
+        expected_result <= X"0000000000000000000000000000000000000000000000000000000000000000";
+        OK_general <= OK_general and ok_v;
+        wait for 4 ns;
+
+        -- Test Case 1 b: Simple case
+        T_Blakley_Value1 <= X"0000000000000000000000000000000000000000000000000000000000000001";
+        T_Blakley_Value2 <= X"0000000000000000000000000000000000000000000000000000000000000001";
+        T_Blakley_Mod <= (others => '1');
+        T_InReady <= '1';
+        wait for 2 ns;
+        T_InReady <= '0';
+        wait until T_Outready = '1';
+        assert T_OutBlackley = X"0000000000000000000000000000000000000000000000000000000000000001" report "Test Case 1 Failed" severity warning;
+        expected_result <= X"0000000000000000000000000000000000000000000000000000000000000001";
+        OK <= T_OutBlackley = X"0000000000000000000000000000000000000000000000000000000000000001";
+        ok_v := T_OutBlackley = X"0000000000000000000000000000000000000000000000000000000000000001";
+        OK_general <= OK_general and ok_v;
+        wait for 4 ns;
+
+        -- Test Case 2: Edge case with maximum values
+        T_Blakley_Value1 <= '0' & (254 downto 0 => '1');
+        T_Blakley_Value2 <= '0' & (254 downto 0 => '1');
+        T_Blakley_Mod <= (others => '1');
+        T_InReady <= '1';
+        wait for 2 ns;
+        T_InReady <= '0';
+        wait until T_Outready = '1';
+        assert unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod) report "Test Case 2 Failed" severity warning;
+        expected_result <= (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK <= unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_v := unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_general <= OK_general and ok_v;
+        wait for 4 ns;
+
+        -- Test Case 3: Random values
+        T_Blakley_Value1 <= X"0000000000000000000000000000000000000000000000000000000000000001";
+        T_Blakley_Value2 <= X"0000000000000000000000000000000000000000000000000000000000000002";
+        T_Blakley_Mod <= X"0000000000000000000000000000000000000000000000000000000000000003";
+        T_InReady <= '1';
+        wait for 2 ns;
+        T_InReady <= '0';
+        wait until T_Outready = '1';
+        assert unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod) report "Test Case 3 Failed" severity error;
+        expected_result <= (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK <= unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_v := unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_general <= OK_general and ok_v;
+        -- assert T_OutBlackley = X"0000000000000000000000000000000000000000000000000000000000000002" report "Test Case 3 Failed" severity warning;
+        wait for 4 ns;
+
+        -- Test Case 4: Large values with different mod
+        T_Blakley_Value1 <= X"0cea1651ef44be1f1f1476b7539bed10d73e3aac782bd9999a1e5a790932bfe9";
+        T_Blakley_Value2 <= X"0cea1651ef44be1f1f1476b7539bed10d73e3aac782bd9999a1e5a790932bfe9";
+        T_Blakley_Mod    <= X"99925173ad65686715385ea800cd28120288fc70a9bc98dd4c90d676f8ff768d";
+        T_InReady <= '1';
+        wait for 2 ns;
+        T_InReady <= '0';
+        wait until T_Outready = '1';
+        assert unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod) report "Test Case 4 Failed" severity error;
+        expected_result <= (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK <= unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_v := unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_general <= OK_general and ok_v;
+        -- assert T_OutBlackley = X"0000000000000000000000000000000000000000000000000000000000000000" report "Test Case 4 Failed" severity error;
+        wait for 4 ns;
+
+        -- Test Case 5: Large values with prime mod
+        T_Blakley_Value1 <= X"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA"; -- Value1 < Mod
+        T_Blakley_Value2 <= X"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF9"; -- Value2 < Mod
+        T_Blakley_Mod <= X"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFB";
+        T_InReady <= '1';
+        wait for 2 ns;
+        T_InReady <= '0';
+        wait until T_Outready = '1';
+        assert unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod) report "Test Case 5 Failed" severity error;
+        expected_result <= (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK <= unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_v := unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_general <= OK_general and ok_v;
+        wait for 4 ns;
+
+        -- Test Case 6: Large values with non-trivial mod
+        T_Blakley_Value1 <= X"123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEEF"; -- Value1 < Mod
+        T_Blakley_Value2 <= X"FEDCBA9876543210FEDCBA9876543210FEDCBA9876543210FEDCBA987654320F"; -- Value2 < Mod
+        T_Blakley_Mod <= X"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0"; -- Mod > Values
+        T_InReady <= '1';
+        wait for 2 ns;
+        T_InReady <= '0';
+        wait until T_Outready = '1';
+        assert unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod) report "Test Case 6 Failed" severity error;
+        expected_result <= (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK <= unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_v := unsigned(T_OutBlackley) = (unsigned(T_Blakley_Value1) * unsigned(T_Blakley_Value2)) mod unsigned(T_Blakley_Mod);
+        OK_general <= OK_general and ok_v;
+        -- wait for 4 ns;
+
+        -- End of tests
+        wait for 2 ns;
+        Done <= true;
+        wait;
+    end process;
+
+end architecture TEST;
--- notre_RSA/src/blocks/Blackley.vhd
+++ notre_RSA/src/blocks/Blackley.vhd
@@ -8,7 +8,6 @@ entity Blackley is
         RESET : in std_logic;
         CLOCK : in std_logic;
         In_Blakley_Value1, In_Blakley_Value2, In_Blakley_Mod : in std_logic_vector(255 downto 0);
-        -- Manual_Reset, 
         Input_Ready : in std_logic;
 
         Out_Blackley : out std_logic_vector(255 downto 0);
@@ -16,62 +15,118 @@ entity Blackley is
     );
 end entity;
 
-architecture blackley_archi of Blackley is
+architecture blackley_archi_v2 of Blackley is
     signal Reg_R : std_logic_vector(255 downto 0) := (others => '0');
     signal Reg_Value1 : std_logic_vector(255 downto 0) := (others => '0');
     signal Reg_Value2 : std_logic_vector(255 downto 0) := (others => '0');
     signal Reg_Mod : std_logic_vector(255 downto 0) := (others => '0');
+    signal i : integer := 1;  -- signal i to replace the for loop
 begin
     process(CLOCK, RESET)
-        variable shifted_R, modulo_R_1, modulo_R_2 : unsigned(255 downto 0);
-        variable shifted_mod : unsigned(255 downto 0);
+        variable shifted_R : unsigned(257 downto 0); -- 257 and not 255 because we need to add one bit for the carry
+        variable modulo_R_1, modulo_R_2, shifted_mod : signed(258 downto 0); -- 258 and not 255 because we need to add one bit for the carry and one bit for the sign
     begin
         if RESET = '1' then
             Reg_R <= (others => '0');
+            i <= 0;  -- Reset i when RESET is active
+            Result_ready <= '0';
         elsif rising_edge(CLOCK) then
-            -- if Manual_Reset = '1' then
-            --     Reg_R <= (others => '0');
-            -- else
-                for i in 1 to 255 loop
-                    if Input_Ready = '1' then
-                        Reg_Value1 <= In_Blakley_Value1;
-                        Reg_Value2 <= In_Blakley_Value2;
-                        Reg_Mod <= In_Blakley_Mod;
-                    else 
-                        Reg_Value1 <= Reg_Value1;
-                        Reg_Value2 <= Reg_Value2(254 downto 0) & '0';
-                        Reg_Mod <= Reg_Mod;
-                    end if;
-                    shifted_R := Reg_R(254 downto 0) & '0';
-                    if Reg_Value2(255) = '1' then
-                        shifted_R := shifted_R + unsigned(Reg_Value1);
-                    end if;
+            if Input_Ready = '1' then
+                Reg_Value1 <= In_Blakley_Value1;
+                Reg_Value2 <= In_Blakley_Value2;
+                Reg_Mod <= In_Blakley_Mod;
+                i <= 0; 
+                Reg_R <= (others => '0');
+                Result_ready <= '0';
+            elsif i <= 255 then
+                Result_ready <= '0';
+                
+                Reg_Value2 <= Reg_Value2(254 downto 0) & '0';
 
-                    -- First Way to do the modulo
-                    if shifted_R >= unsigned(Reg_Mod) then
-                        shifted_R := shifted_R - unsigned(Reg_Mod);
-                    end if;
-                    if unsigned(shifted_R) >= unsigned(Reg_Mod) then
-                        shifted_R := shifted_R - unsigned(Reg_Mod);
-                    end if;
-                    Reg_R <= std_logic_vector(shifted_R);
+                shifted_R := unsigned('0' & Reg_R(255 downto 0) & '0');
+                if Reg_Value2(255) = '1' then
+                    shifted_R := shifted_R + unsigned(Reg_Value1);
+                end if;
 
-                    -- Second Way to do the modulo 
-                    -- -> if not choosen remove modulo_R_1 and modulo_R_2 and shifted_mod
-                    shifted_mod := unsigned(Reg_Mod(254 downto 0) & '0');
-                    modulo_R_1 := shifted_R - unsigned(Reg_Mod);
-                    modulo_R_2 := shifted_R - shifted_mod;
-                    if modulo_R_2 >= 0 then
-                        Reg_R <= std_logic_vector(modulo_R_2);
-                    elsif modulo_R_1 >= 0 then
-                        Reg_R <= std_logic_vector(modulo_R_1);
-                    else
-                        Reg_R <= std_logic_vector(shifted_R);
-                    end if;
-                end loop;
+                -- First Way to do the modulo
+                
+                    -- if shifted_R >= unsigned(Reg_Mod) then
+                    --     shifted_R := shifted_R - unsigned(Reg_Mod);
+                    -- end if;
+                    -- if unsigned(shifted_R) >= unsigned(Reg_Mod) then
+                    --     shifted_R := shifted_R - unsigned(Reg_Mod);
+                    -- end if;
+                    -- Reg_R <= std_logic_vector(shifted_R);
+
+                -- Second Way to do the modulo 
+                -- -> if not chosen remove modulo_R_1 and modulo_R_2 and shifted_mod
+                shifted_mod := signed("00" & Reg_Mod(255 downto 0) & '0');
+                modulo_R_1 := signed('0' & shifted_R) - signed("00" & Reg_Mod);
+                modulo_R_2 := signed('0' & shifted_R) - shifted_mod;
+                if modulo_R_2 >= 0 then
+                    Reg_R <= std_logic_vector(modulo_R_2(255 downto 0));
+                elsif modulo_R_1 >= 0 then
+                    Reg_R <= std_logic_vector(modulo_R_1(255 downto 0));
+                else
+                    Reg_R <= std_logic_vector(shifted_R(255 downto 0));
+                end if;
+
+                i <= i + 1;
+            else
                 Result_ready <= '1';
                 Out_Blackley <= Reg_R;
-            -- end if;
+            end if;
+        end if;
+    end process;
+end architecture;
+
+architecture blackley_archi_v1 of Blackley is
+    signal Reg_R : std_logic_vector(255 downto 0) := (others => '0');
+    signal Reg_Value1 : std_logic_vector(255 downto 0) := (others => '0');
+    signal Reg_Value2 : std_logic_vector(255 downto 0) := (others => '0');
+    signal Reg_Mod : std_logic_vector(255 downto 0) := (others => '0');
+    signal i : integer := 1;  -- signal i to replace the for loop
+begin
+    process(CLOCK, RESET)
+        variable shifted_R : unsigned(257 downto 0); -- 257 and not 255 because we need to add one bit for the carry
+    begin
+        if RESET = '1' then
+            Reg_R <= (others => '0');
+            i <= 0;  -- Reset i when RESET is active
+            Result_ready <= '0';
+        elsif rising_edge(CLOCK) then
+            if Input_Ready = '1' then
+                Reg_Value1 <= In_Blakley_Value1;
+                Reg_Value2 <= In_Blakley_Value2;
+                Reg_Mod <= In_Blakley_Mod;
+                i <= 0; 
+                Reg_R <= (others => '0');
+                Result_ready <= '0';
+            elsif i <= 255 then
+                Result_ready <= '0';
+                
+                Reg_Value2 <= Reg_Value2(254 downto 0) & '0';
+
+                shifted_R := unsigned('0' & Reg_R(255 downto 0) & '0');
+                if Reg_Value2(255) = '1' then
+                    shifted_R := shifted_R + unsigned(Reg_Value1);
+                end if;
+
+                -- First Way to do the modulo
+                
+                if shifted_R >= unsigned(Reg_Mod) then
+                    shifted_R := shifted_R - unsigned(Reg_Mod);
+                end if;
+                if shifted_R >= unsigned(Reg_Mod) then
+                    shifted_R := shifted_R - unsigned(Reg_Mod);
+                end if;
+                Reg_R <= std_logic_vector(shifted_R(255 downto 0));
+
+                i <= i + 1;
+            else
+                Result_ready <= '1';
+                Out_Blackley <= Reg_R;
+            end if;
         end if;
     end process;
 end architecture;
