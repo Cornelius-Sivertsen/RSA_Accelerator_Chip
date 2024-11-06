abed62d3e1c9411fdc27cd8d42bfe292dcf34195
Thibault Pouyanne <93395244+Tezyy@users.noreply.github.com>
2024-11-06 17:00:05 +0100

    change

--- Exponentiation/source/exponentiation.vhd
+++ Exponentiation/source/exponentiation.vhd
@@ -7,26 +7,26 @@ entity exponentiation is
 	);
 	port (
 		--input controll
-		valid_in	: in STD_LOGIC;
-		ready_in	: out STD_LOGIC;
+		valid_in	: in STD_LOGIC; --msgin_valid
+		ready_in	: out STD_LOGIC; -- msgin_ready
 
 		--input data
-		message 	: in STD_LOGIC_VECTOR ( C_block_size-1 downto 0 );
-		key 		: in STD_LOGIC_VECTOR ( C_block_size-1 downto 0 );
+		message 	: in STD_LOGIC_VECTOR ( C_block_size-1 downto 0 ); --msgin_data
+		key 		: in STD_LOGIC_VECTOR ( C_block_size-1 downto 0 ); --key_e
 
 		--ouput controll
-		ready_out	: in STD_LOGIC;
-		valid_out	: out STD_LOGIC;
+		ready_out	: in STD_LOGIC; --msgout_ready
+		valid_out	: out STD_LOGIC; --msgout_valid
 
 		--output data
-		result 		: out STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+		result 		: out STD_LOGIC_VECTOR(C_block_size-1 downto 0); --msgout_data
 
 		--modulus
-		modulus 	: in STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+		modulus 	: in STD_LOGIC_VECTOR(C_block_size-1 downto 0); --key_n
 
 		--utility
 		clk 		: in STD_LOGIC;
-		reset_n 	: in STD_LOGIC
+		reset_n 	: in STD_LOGIC 
 	);
 end exponentiation;
 
@@ -37,7 +37,7 @@ architecture expBehave of exponentiation is
     signal E_nxt            : std_logic_vector(C_block_size-1 downto 0);
     signal C_nxt            : std_logic_vector(C_block_size-1 downto 0);
 
-    signal trigger_reg          : std_logic; -- register for the trigger signal from the FSM
+    signal trigger_reg_sig          : std_logic; -- register for the trigger signal from the FSM
 
 
     -- Blakely signals
@@ -45,10 +45,13 @@ architecture expBehave of exponentiation is
     signal Sortie_blk_2     : std_logic_vector(C_block_size-1 downto 0);
     -- input
     signal Input_blk_1_ready : std_logic;
+    signal Input_blk_2_ready : std_logic;
+
     --output
     signal Ready_blk_1      :std_logic;
     signal Ready_blk_2      :std_logic;
 
+    signal valid_out_sig    :std_logic;
 	--register shift-left
 	signal trig_first_time_only : std_logic; --to shift left the exponent
 											 -- after 1st use of his MSF to initialize register C
@@ -60,14 +63,15 @@ architecture expBehave of exponentiation is
     --signal msgin_ready : std_logic;
     --signal msgin_valid : std_logic;
     
-    signal internal_reset : std_logic;
+    --signal internal_reset : std_logic;
+    signal manual_reset : std_logic;
 
 	--result <= message xor modulus;
 	--ready_in <= ready_out;
 	--valid_out <= valid_in;
 
     begin
-        BLACKLEY_1 : entity work.Blackley
+        BLACKLEY_1 : entity work.Blackley(blackley_archi_v2)
             port map (
 
                 RESET                       => reset_n  ,
@@ -75,13 +79,11 @@ architecture expBehave of exponentiation is
                 In_Blakley_Value1           => C_r      ,
                 In_Blakley_Value2           => C_r      ,
                 In_Blakley_Mod              => modulus  ,
-                --Manual_Reset                => -- FINIR
                 Input_Ready                 => Input_blk_1_ready ,
-
                 Out_Blackley                => Sortie_blk_1 ,
                 Result_ready                => Ready_blk_1
             );
-        BLACKLEY_2 : entity work.Blackley
+        BLACKLEY_2 : entity work.Blackley(blackley_archi_v2)
             port map (
 
                 RESET                       => reset_n  ,
@@ -89,33 +91,31 @@ architecture expBehave of exponentiation is
                 In_Blakley_Value1           => Sortie_blk_1 ,
                 In_Blakley_Value2           => message      ,
                 In_Blakley_Mod              => modulus      ,
-                --Manual_Reset                => -- FINIR
                 Input_Ready                 => Ready_blk_1 and MSF_exponent,
-
                 Out_Blackley                => Sortie_blk_2,
                 Result_ready                => Ready_blk_2 
             );
 
-        REGISTER_E : entity work.register_shift_left
+        REGISTER_E : entity work.Shift_Register_With_MSF
             port map(
                 CLK       	=> clk,
-                RESET     	=> reset_n,
+                RESET     	=> reset_n or manual_reset,
                 CONTROL 	=> fill_E_and_C, -- a update chaque fois qu'un message est fini
-                TRIG    	=> trigger_reg or trig_first_time_only,
+                TRIG    	=> trigger_reg_sig or trig_first_time_only,
                 DATA_IN 	=> key,
                 MSF_OUT  	=> MSF_exponent -- on doit commencer à lenght-2 voir algo
             );
         
-        FSM : entity work.mod_exp_fsm
+        FSM : entity work.exp_fsm
             port map(
                 reset => reset_n,
                 clock => clk,
-                trigger => trigger_reg,
+                trigger => trigger_reg_sig,
                 msgin_valid => valid_in,
                 write_back_resolution => fill_E_and_C,
                 manual_reset => manual_reset,
                 msgin_ready =>ready_in,
-                msgout_valid =>ready_out
+                msgout_valid =>valid_out_sig
                 
             );
 
@@ -123,40 +123,47 @@ architecture expBehave of exponentiation is
     
 
     process(clk, reset_n)
+     variable trigger_reg : std_logic := '0';
+     variable valid_out_var :std_logic :='0';
         begin
-            if ((reset_n = '1') or (internal_reset = '1')) then
+            if ((reset_n = '1') or (manual_reset = '1')) then
                 C_r <= (0 => '1', others => '0');   -- Reset C_r to a known value
-                trigger_reg <= '0';
+                trigger_reg := '0';
                 Input_blk_1_ready <= '0';
                 Input_blk_2_ready <= '0';
             elsif rising_edge(clk) then
-                internal_reset <= ready_out and valid_in ;
-                
-                if Ready_blk_1 ='1' then            -- Check if we need to do Blackey_2
-                    if MSF_exponent /='1' then      
-                        C_nxt <= Sortie_blk_1;      
-                        trigger_reg <= '1';
-                    elsif Ready_blk_2 ='1' then     -- Waiting for Blackley_2 to be finished
-                        C_nxt <= Sortie_blk_2;
-                        trigger_reg <= '1';
-                    end if;
-                else
-                    trigger_reg <= '0';             -- Reset trigger if Ready_blk_1 is not 1  
-                end if;
-                
                 -- update of C_r for each new message
 				-- done once per message
                 if fill_E_and_C = '1' and valid_in ='1' then
 					if MSF_exponent /= '0' then
 						C_r <= message;             --update C_r with the message value
-					elsif MSF_exponent = '0' then
-                        C_r <= (others => '0');     -- if the signal is out of the fsm only the 1st time or not changes everything
-                        C_r(0) <= '1';
-                        end if;
-					trig_first_time_only <= '1';
+					else 
+                        C_r <= (255 downto 1 => '0') & '1';     -- if the signal is out of the fsm only the 1st time or not changes everything
+                       -- C_r(0) <= '1';
+                    end if;
+					trig_first_time_only <= '1'; 
+                    Input_blk_1_ready <= '1';
 				else 
 					trig_first_time_only <= '0';
 				end if;
+                
+                
+                if Ready_blk_1 ='1' then            -- Check if we need to do Blackey_2
+                    if MSF_exponent /='1' then      
+                        C_nxt <= Sortie_blk_1;      
+                        trigger_reg := '1';
+                    elsif Ready_blk_2 ='1' then     -- Waiting for Blackley_2 to be finished
+                        C_nxt <= Sortie_blk_2;
+                        trigger_reg := '1';
+                    else 
+                        trigger_reg :='0';
+                    end if;
+                else
+                    trigger_reg := '0';             -- Reset trigger if Ready_blk_1 is not 1  
+                end if;
+                
+                trigger_reg_sig <= trigger_reg;
+                valid_out_var := valid_out_sig;
 
 				if trigger_reg = '1' then
                     C_r <= C_nxt;                    --update C_r with new calculated value
@@ -168,11 +175,11 @@ architecture expBehave of exponentiation is
                 -- OUT(PUTE)
                 -- gérer la remise à 0 de ready_out
                 -- qu'il se remette à 0 au bon moment
-                if (ready_out ='1') then
+                if (valid_out_var = '1') then
                     result <= C_r;                  -- assign C_r to result when calculation done
                 else
                     result <= (others => '0');      --output zeros if not finished
                 end if;
             end if;
         end process;
-
+end architecture ;
--- notre_RSA/src/utilities/register_shift_left.vhd
+++ notre_RSA/src/utilities/register_shift_left.vhd
@@ -1,9 +1,29 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 29.10.2024 20:40:02
+-- Design Name: 
+-- Module Name: register_shift_left - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.ALL;
 
 ENTITY Shift_Register_With_MSF IS
     generic(
-        N: positive := 256; 
+        N: positive := 256
     );
     PORT (
         CLK       : IN  STD_LOGIC;
@@ -19,6 +39,7 @@ END entity;
 
 ARCHITECTURE behavior OF Shift_Register_With_MSF IS
     signal Reg: std_logic_vector(N-1 downto 0) := (others => '0');  -- Registre interne
+    --signal test      : std_logic := '0';
 BEGIN
 
     process (CLK, RESET)
@@ -30,10 +51,14 @@ BEGIN
                 Reg <= DATA_IN;
             elsif (TRIG ='1') then
                 Reg <= Reg(N-2 downto 0) & '0'; --shift left by one bite
-            else 
+                --test <= '1';
+            else
                 Reg <= Reg;
+                --test <= '0';
             end if;
         end if;
     end process;
     MSF_OUT <= Reg(N-1);
+
 END ARCHITECTURE;
+
