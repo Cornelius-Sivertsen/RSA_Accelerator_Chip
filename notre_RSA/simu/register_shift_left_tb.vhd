8fba05a21968d3af516e52439c5e4dd031d56d0f
Thibault Pouyanne <93395244+Tezyy@users.noreply.github.com>
2024-11-06 15:03:57 +0100

    comments

--- notre_RSA/simu/register_shift_left_tb.vhd
+++ notre_RSA/simu/register_shift_left_tb.vhd
@@ -1,32 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 29.10.2024 20:42:34
--- Design Name: 
--- Module Name: register_shift_left_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
